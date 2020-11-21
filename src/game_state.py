4aa043d3f3cc67006dc7253ddda5c07462ad7d9d
Gabriel <gabrielnsdal@gmail.com>
2020-11-20 23:23:19 -0300

    Alterações

---
+++ src/game_state.py
@@ -0,0 +1,41 @@
+class Game_sate:
+    def __init__(self,agent,game):
+        self._agent = agent
+        self._game = game
+    def get_state(self,actions):
+        score = self._game.get_score() 
+        reward = 0.1*score/10 # dynamic reward calculation
+        is_over = False #game over
+        if actions[1] == 1: #else do nothing
+            self._agent.jump()
+            reward = 0.1*score/11
+        image = grab_screen() 
+        
+        if self._agent.is_crashed():
+            self._game.restart()
+            reward = -11/score
+            is_over = True
+        return image, reward, is_over #return the Experience tuple
+'''
+get_state(): accepts an array of actions, 
+             performs the action on the agent 
+returns :  new state, reward and if the game ended.
+'''
+class Game_sate:
+    def __init__(self,agent,game):
+        self._agent = agent
+        self._game = game
+    def get_state(self,actions):
+        score = self._game.get_score() 
+        reward = 0.1*score/10 # dynamic reward calculation
+        is_over = False #game over
+        if actions[1] == 1: #else do nothing
+            self._agent.jump()
+            reward = 0.1*score/11
+        image = grab_screen() 
+        
+        if self._agent.is_crashed():
+            self._game.restart()
+            reward = -11/score
+            is_over = True
+        return image, reward, is_over #return the Experience tuple
